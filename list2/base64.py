1efcc10e632ad1e7705fdc5a94543cc3dcfb4afd
Jakub Dąbek <jakub.dabek@gmail.com>
2020-03-10 13:04:38 +0100

    Fix base64 algorithm

--- list2/base64.py
+++ list2/base64.py
@@ -6,7 +6,7 @@ IncompleteTriple = Tuple[T, Optional[T], Optional[T]]
 Quad = Tuple[T, T, T, T]
 
 
-base64_alphabet = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+base64_alphabet = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
 
 
 def bytes_from_file(f: BinaryIO, chunk_size: int = 8192) -> Iterator[int]:
@@ -49,19 +49,17 @@ def encode_single(v: int) -> int:
 
 def encode_triple(triple: IncompleteTriple[int]) -> Quad[int]:
     print([bin(x) for x in triple if x is not None])
-    values: List[int] = [encode_single(triple[0] >> 2)]
-    current: int = (triple[0] << 4) & 0b_0011_0000
+    x1 = triple[0] >> 2
+    x2 = (triple[0] << 4) & 0b_0011_0000
     if triple[1] is None:
-        return values[0], encode_single(current), ord('='), ord('=')
-    current |= triple[1] >> 4
-    values.append(encode_single(current))
-    current = (triple[1] & 0b_0000_1111) << 2
+        return tuple(map(encode_single, (x1, x2, -1, -1)))
+    x2 |= triple[1] >> 4
+    x3 = (triple[1] & 0b_0000_1111) << 2
     if triple[2] is None:
-        return values[0], values[1], encode_single(current), ord('=')
-    current |= triple[2] >> 6
-    values.append(encode_single(current))
-    values.append(encode_single(triple[2] & 0b_0011_1111))
-    return tuple(values)
+        return tuple(map(encode_single, (x1, x2, x3, -1)))
+    x3 |= triple[2] >> 6
+    x4 = triple[2] & 0b_0011_1111
+    return tuple(map(encode_single, (x1, x2, x3, x4)))
 
 
 def decode_single(v: int) -> int:
@@ -70,22 +68,23 @@ def decode_single(v: int) -> int:
 
 def decode_quad(quad: Quad[int]) -> IncompleteTriple[int]:
     print(bytes(quad))
-    current = decode_single(quad[0]) << 2
-    current |= decode_single(quad[1]) >> 4
-    values: List[int] = [current]
-    current = (decode_single(quad[1]) & 0b_0000_1111) << 4
-    if quad[2] == '=':
-        return values[0], current, None
-    current |= decode_single(quad[2]) >> 2
-    if quad[3] == '=':
-        if decode_single(quad[2]) & 0b11 != 0:
+    x1 = decode_single(quad[0]) << 2
+    q1 = decode_single(quad[1])
+    x1 |= q1 >> 4
+    x2 = (q1 & 0b_0000_1111) << 4
+    if quad[2] == ord('='):
+        if quad[3] != ord('='):
             raise ValueError("invalid base64 string")
-        return values[0], current, None
-    values.append(current)
-    current = (decode_single(quad[2]) & 0b11) << 6
-    current |= decode_single(quad[3])
-    values.append(current)
-    return tuple(values)
+        return x1, x2, None
+    q2 = decode_single(quad[2])
+    x2 |= q2 >> 2
+    if quad[3] == ord('='):
+        if q2 & 0b11 != 0:
+            raise ValueError("invalid base64 string")
+        return x1, x2, None
+    x3 = (q2 & 0b11) << 6
+    x3 |= decode_single(quad[3])
+    return x1, x2, x3
 
 
 def main():
@@ -102,6 +101,7 @@ def main():
             for quad in quads_assert(bytes_from_file(in_file)):
                 decoded = decode_quad(quad)
                 length = sum(1 for x in decoded if x is not None)
+                print(decoded)
                 out_file.write(bytes(decoded[:length]))
     else:
         with open(args.input, 'rb') as in_file, open(args.output, 'wb') as out_file:
